666a7328ed4524846c5e4a621c07f0cc327f34f7
guiemiliano <guiemiliano.dev@gmail.com>
2024-12-30 23:34:50 -0300

    first commit

---
+++ src/main/java/dev/java10x/CadastroDeNinjas2/CadastroDeNinjas2Application.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeNinjas2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjas2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjas2Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/CadastroDeNinjas2/CadastroDeNinjas2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeNinjas2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjas2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
