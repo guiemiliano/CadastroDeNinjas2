8147c7f4c32570ea7ef6d89d85f26e1db7d47dda
guiemiliano <guiemiliano.dev@gmail.com>
2025-01-06 16:45:08 -0300

    build: Conecta o banco de dados H2 a minha aplicação Spring

--- src/main/java/dev/java10x/CadastroDeNinjas2/CadastroDeNinjas2Application.java
+++ src/main/java/dev/java10x/CadastroDeNinjas2/CadastroDeNinjas2Application.java
@@ -3,6 +3,7 @@ package dev.java10x.CadastroDeNinjas2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CadastroDeNinjas2Application {
 
