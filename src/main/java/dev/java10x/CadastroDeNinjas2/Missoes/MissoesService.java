d9a25b3c3672770ac4830c61c246b36b65ca66b5
guiemiliano <guiemiliano.dev@gmail.com>
2025-01-26 10:27:34 -0300

    feat: Rota de findById e findAll adicionada

--- src/main/java/dev/java10x/CadastroDeNinjas2/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas2/Missoes/MissoesService.java
@@ -1,4 +1,26 @@
 package dev.java10x.CadastroDeNinjas2.Missoes;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MissoesService {
+
+    private MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository){
+        this.missoesRepository = missoesRepository;
+    }
+
+    public List<MissoesModel>listarMissoes(){
+        return missoesRepository.findAll();
+    }
+
+    public MissoesModel listarMissoesPorId(Long id){
+        Optional<MissoesModel> missoesPorId = missoesRepository.findById(id);
+        return missoesPorId.orElse(null);
+    }
+
 }
